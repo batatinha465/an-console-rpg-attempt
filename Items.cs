cb50fe5c993c244bf58db3f31443aa6e0fd1e5f0
batatinha465 <migueladri2006@gmail.com>
2024-06-03 02:45:16 -0300

    CreatedItemClass

---
+++ Items.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TentativaDeRPG
+{
+    internal class Items
+    {
+    }
+}
