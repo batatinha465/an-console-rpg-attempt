88c5ea93996ff5bc5b892b105fe3e154d40164b8
batatinha465 <migueladri2006@gmail.com>
2024-06-03 02:10:36 -0300

    Refazendo tudo

---
+++ TentativaDeRpg/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TentativaDeRpg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
